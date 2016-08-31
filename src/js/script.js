4958c21eaff9558f45bd7696c5c4885f6451595f
Green Lera <zelnayavaleriya@gmail.com>
2016-08-31 20:15:59 +0300

    //

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,25 @@
 $(document).ready(function(){
-  // весь ваш код
+
+  //Промо в верхней части
+  $("#promo").owlCarousel({
+  	items: 1,
+  });
+
+  //Галерея на главной
+  var gallery = $('#gallery');
+  gallery.owlCarousel({
+  	items: 4,
+  	loop: true,
+  	center: true,
+  });
+
+  gallery.on('changed.owl.carousel', function(event){
+  	
+  });
+
+  $('#gallery img[data-large-img-url]').on('click', function(){
+  	$('#gallery-large-img').attr('src', $(this).data('large-img-url') );
+  });
+
+
 });
